c6d4dbfcf8b0ecfc2c98d4cbff82681db302a156
Kaoue Cavalcanti <knc@tuta.io>
2026-03-20 00:21:32 -0300

    fix: correct typo in deploy-service.sh and remove unnecessary shebang from local.env.example

--- config/defaults/services/service-template/deploy-service.sh
+++ config/defaults/services/service-template/deploy-service.sh
@@ -10,4 +10,4 @@ bash ./scripts/inventory-generate.sh service-template
 bash ./scripts/run-playbook.sh service-template 00-initial.yml
 bash ./scripts/run-playbook.sh service-template 10-base-security.yml
 
-# Run onther playbooks or actions as needed
+# Run other playbooks or actions as needed
